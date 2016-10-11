829c2c27d225372b2d91ca7b657f702eaff91382
Aaron Latham <drummerstrike@gmail.com>
2016-10-11 16:35:38 -0600

    done

--- server.js
+++ server.js
@@ -1,1 +1,62 @@
+var express = require("express");
+var bodyParser = require("body-parser");
+var app = express();
+app.use(bodyParser.json());
 
+
+
+var messages = ["hey there","what up?"];
+
+app.get('/', function( req, res ) {
+  res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+   }).send(JSON.stringify(messages));
+});
+
+
+
+app.post('/', function( req, res ) {
+  messages.push(req.body.message);
+
+    res.status(200).set({
+    'Content-Type': 'application/json',
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+    'X-XSS-Protection': '1; mode=block',
+    'X-Frame-Options': 'SAMEORIGIN',
+    'Content-Security-Policy': "default-src 'self' devmountain.github.io"
+  }).send(JSON.stringify(messages));
+
+});
+
+app.options('/', function( req, res ) {
+
+
+    res.status(200).set({
+    'Access-Control-Allow-Origin': '*',
+    'Access-Control-Allow-Methods': 'OPTIONS, GET, POST',
+    'Access-Control-Allow-Headers': 'Origin, X-Requested-With, Content-Type, Accept',
+
+  }).send();
+
+});
+
+
+
+
+
+
+
+
+
+
+app.listen(8989,function(){
+  console.log("listening on port 8989")
+});
